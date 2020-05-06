b91d73bb4b8fc47f6fda04ca6d44abe55a64802e
MAC <apple@MACs-MacBook-Pro.local>
2020-05-05 21:18:14 -0300

    Corrección de meta tags

--- resources/views/web/partials/head.blade.php
+++ resources/views/web/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-<meta name="description" content="koira - Factory and Manufacturing Html Template">
+{{--<meta name="description" content="koira - Factory and Manufacturing Html Template">--}}
 <link href="{!! asset('template-web/assets/images/favicon/favicon.png') !!}" rel="icon">
 <title>Eventum - La tecnología al alcance de tu mano.</title>
 <link rel="stylesheet"
