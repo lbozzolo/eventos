d82839a50ce216205e7894252c25e73543dde817
MAC <apple@MACs-MacBook-Pro.local>
2020-05-08 19:27:24 -0300

    eliminación de preview en widgets de video e iframe

--- resources/views/proyectos/partials/widget-videos.blade.php
+++ resources/views/proyectos/partials/widget-videos.blade.php
@@ -5,21 +5,21 @@
         <h3 class="d-inline-block mr-3">Videos</h3>
         <a href="{!! route($modelPlural.'.videos', $item->id) !!}" class="btn btn-outline-success btn-sm float-right">Agregar</a>
 
-        <div class="card-body mt-3" style="background-color: #f2f8f9">
+        {{--<div class="card-body mt-3" style="background-color: #f2f8f9">--}}
 
-            <div class="row">
-                @forelse($item->videos as $video)
+            {{--<div class="row">--}}
+                {{--@forelse($item->videos as $video)--}}
 
-                    <img src="https://img.youtube.com/vi/{!! $video->video_id !!}/default.jpg" style="width: 100px; margin: 5px"><br>
+                    {{--<img src="https://img.youtube.com/vi/{!! $video->video_id !!}/default.jpg" style="width: 100px; margin: 5px"><br>--}}
 
-                @empty
+                {{--@empty--}}
 
-                    <span class="text-muted"><i class="mdi mdi-vanish"></i> <small><em>No hay videos para mostrar.</em></small> </span>
+                    {{--<span class="text-muted"><i class="mdi mdi-vanish"></i> <small><em>No hay videos para mostrar.</em></small> </span>--}}
 
-                @endforelse
-            </div>
+                {{--@endforelse--}}
+            {{--</div>--}}
 
-        </div>
+        {{--</div>--}}
 
     </div>
 
