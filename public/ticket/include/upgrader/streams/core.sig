48d87f66d675100f12995c57d465117afdecba4d
MAC <apple@MACs-MacBook-Pro.local>
2020-08-03 14:20:20 -0300

    Agregado de script de soporte

--- public/ticket/include/upgrader/streams/core.sig
+++
@@ -1,1 +0,0 @@
-4bd47d94b10bd8a6bab35c119dadf41f
--- resources/views/web/ingresar-charla.blade.php
+++ resources/views/web/ingresar-charla.blade.php
@@ -336,4 +336,21 @@
 
     </script>
 
+    <!-- GetButton.io widget -->
+    <script type="text/javascript">
+        (function () {
+            var options = {
+                email: "soporte@eventum.com.ar", // Email
+                call_to_action: "Eventum Soporte", // Call to action
+                button_color: "#97B128", // Color of button
+                position: "right", // Position may be 'right' or 'left'
+            };
+            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+        })();
+    </script>
+    <!-- /GetButton.io widget -->
+
 @endsection
