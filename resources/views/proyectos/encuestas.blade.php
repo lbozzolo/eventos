fcf463ae657ea3f9dc60d9d27959a0058b70a903
MAC <apple@MACs-MacBook-Pro.local>
2020-08-18 19:24:45 -0300

    ABM de encuestas

--- resources/views/proyectos/encuestas.blade.php
+++ resources/views/proyectos/encuestas.blade.php
@@ -10,47 +10,47 @@
                 <div class="card-body">
                     <h2>
                         {!! $item->nombre !!} / <span class="text-warning">Encuestas</span>
-                        {{--@can('crear_encuestas')--}}
-                        {{--<button title="Agregar" type="button" data-toggle="modal" data-target="#create{!! $item->id !!}" class="btn btn-primary">Agregar</button>--}}
-                        {{--@endcan--}}
+                        @can('crear_encuestas')
+                        <button title="Agregar" type="button" data-toggle="modal" data-target="#create{!! $item->id !!}" class="btn btn-primary">Agregar</button>
+                        @endcan
                         <a href="{!! route('proyectos.show', $item->id) !!}" class="btn btn-outline-dark">Volver</a>
                     </h2>
 
-                    {{--@can('crear_encuestas')--}}
-                        {{--<div class="modal fade" id="create{!! $item->id !!}" tabindex="-1" role="dialog" aria-hidden="true">--}}
-                        {{--<div class="modal-dialog" role="document">--}}
-                            {{--<div class="modal-content">--}}
-                                {{--<div class="modal-header">--}}
-                                    {{--<h5 class="modal-title" ><i class="mdi mdi-plus text-success"></i> Agregar {!! $modelSpanish !!}</h5>--}}
-                                    {{--<button type="button" class="close" data-dismiss="modal" aria-label="Close">--}}
-                                        {{--<span aria-hidden="true">&times;</span>--}}
-                                    {{--</button>--}}
-                                {{--</div>--}}
-                                {{--{!! Form::open(['route' => ['encuestas.store', $item->id], 'method' => 'post']) !!}--}}
-                                {{--<div class="modal-body">--}}
+                    @can('crear_encuestas')
+                        <div class="modal fade" id="create{!! $item->id !!}" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" ><i class="mdi mdi-plus text-success"></i> Agregar {!! $modelSpanish !!}</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                {!! Form::open(['route' => ['encuestas.store', $item->id], 'method' => 'post']) !!}
+                                <div class="modal-body">
 
-                                    {{--<div class="form-group">--}}
-                                        {{--{!! Form::label('nombre', 'Nombre de la encuesta') !!}--}}
-                                        {{--{!! Form::text('nombre', null, ['class' => 'form-control']) !!}--}}
-                                    {{--</div>--}}
+                                    <div class="form-group">
+                                        {!! Form::label('nombre', 'Nombre de la encuesta') !!}
+                                        {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
+                                    </div>
 
-                                    {{--<div class="form-group">--}}
-                                        {{--{!! Form::label('iframe_id', '¿A qué Iframe corresponde la encuesta?') !!}--}}
-                                        {{--{!! Form::select('iframe_id', $item->iframes->pluck('title', 'id'),null, ['class' => 'form-control select2', 'placeholder' => 'Todos']) !!}--}}
-                                    {{--</div>--}}
+                                    <div class="form-group">
+                                        {!! Form::label('iframe_id', '¿A qué Iframe corresponde la encuesta?') !!}
+                                        {!! Form::select('iframe_id', $item->iframes->pluck('title', 'id'),null, ['class' => 'form-control select2', 'placeholder' => 'Todos']) !!}
+                                    </div>
 
-                                {{--</div>--}}
-                                {{--<div class="modal-footer">--}}
+                                </div>
+                                <div class="modal-footer">
 
-                                    {{--<button title="Agregar encuesta" type="submit" class="btn btn-sm btn-primary">Aceptar</button>--}}
-                                    {{--<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>--}}
+                                    <button title="Agregar encuesta" type="submit" class="btn btn-sm btn-primary">Aceptar</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
 
-                                {{--</div>--}}
-                                {{--{!! Form::close() !!}--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                    {{--@endcan--}}
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+                    </div>
+                    @endcan
 
                 </div>
 
@@ -64,20 +64,16 @@
                                         <div class="card">
                                             <div class="card-body">
                                                 <h4>{!! $encuesta->nombre !!}</h4>
+
                                                 @if($encuesta->iframe)
-                                                    <p>
-                                                        Iframe:
-                                                        <span class="badge badge-primary">{!! $encuesta->iframe->title !!}</span>
-                                                    </p>
+                                                    <p>Iframe: <span class="badge badge-primary">{!! $encuesta->iframe->title !!}</span> </p>
                                                 @else
                                                     <p>Iframe: todos</p>
                                                 @endif
-                                                {{--<a href="{!! route('encuestas.show', $encuesta->id) !!}" class="btn btn-outline-primary">Configurar</a>--}}
-                                                @if($encuesta->respuestas->count())
-                                                    <a href="{!! route('encuestas.respuestas', $encuesta->id) !!}" class="btn btn-secondary">Respuestas</a>
-                                                @else
-                                                    <button class="btn btn-secondary" disabled>Respuestas</button>
-                                                @endif
+
+                                                <a href="{!! route('encuestas.show', $encuesta->id) !!}" class="btn btn-outline-primary">
+                                                    <i class="mdi mdi-settings"> </i>
+                                                </a>
 
                                                 @can('eliminar_encuestas')
                                                     <button title="Eliminar Encuesta" type="button" data-toggle="modal" data-target="#delete{!! $encuesta->id !!}" class="btn btn-outline-danger">
@@ -110,6 +106,13 @@
                                                         </div>
                                                     </div>
                                                 @endcan
+
+                                                @if($encuesta->respuestas->count())
+                                                    <a href="{!! route('encuestas.respuestas', $encuesta->id) !!}" class="btn btn-secondary">Respuestas</a>
+                                                @else
+                                                    <button class="btn btn-secondary" disabled>Respuestas</button>
+                                                @endif
+
                                             </div>
                                         </div>
                                     </div>
